766bdc7f63498f85de6e4980b9b88ab3815454ba
alaedev23 <alaedev23@gmail.com>
2024-01-10 21:24:26 +0100

    PHP MVC

--- RaffleSpain_MVC/classes/Controller/HomeController.class.php
+++ RaffleSpain_MVC/classes/Controller/HomeController.class.php
@@ -8,11 +8,7 @@ class HomeController {
     
     public function show() {
         
-        require_once 'lang/lang_cookies.php';
-        
-        $fitxerDeTraduccions = "lang/{$lang}.php";
-        
-        HomeView::show($fitxerDeTraduccions );      
+        HomeView::show();    
         
     }
 }
---
+++ RaffleSpain_MVC/classes/View/HomeView.class.php
@@ -0,0 +1,11 @@
+<?php
+
+class HomeView {
+    
+    public static function show() {
+        
+        include "public/html/home.html";     
+        
+    }
+}
+
