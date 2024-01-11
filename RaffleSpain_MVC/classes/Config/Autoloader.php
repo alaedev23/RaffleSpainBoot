082f2c6364a0d3b7b4940e49f6c282cb7abea6b1
polanmarc <polanmarc19@gmail.com>
2024-01-11 19:32:06 +0100

    Correcion Autoloader

--- RaffleSpain_MVC/classes/Config/Autoloader.php
+++ RaffleSpain_MVC/classes/Config/Autoloader.php
@@ -1,28 +1,44 @@
 <?php
-
-class Autoloader
-{
-    private const CARPETAS = ['Model', 'View', 'Controller', 'Config'];
-
-    public static function load($clase)
-    {
+class Autoloader{
+    private const CARPETAS = ['Model', 'View', 'Controller'];
+    // $Config
+    
+    public static function load($clase){
         foreach (self::CARPETAS as $carpeta) {
-            if (file_exists("classes/$carpeta/" . strtolower($clase) . '.class.php')) {
-                include "classes/$carpeta/" . strtolower($clase) . '.class.php';
-                return;
-            }
-        }
-        //         throw new Exception("No s'ha trobat la definicio de la classe $clase");
-    }
-
-    public static function newload($clase)
-    {
-        foreach (self::CARPETAS as $carpeta) {
-            if (file_exists("classes/$carpeta/" . $clase . '.php')) {
-                include "classes/$carpeta/" . $clase . '.php';
+            if (file_exists("classes/$carpeta/".ucfirst($clase).'.class.php')) {
+                include "classes/$carpeta/".ucfirst($clase).'.class.php';
                 return;
             }
         }
         throw new Exception("No s'ha trobat la definicio de la classe $clase");
     }
+    
+    
 }
+
+// class Autoloader
+// {
+//     private const CARPETAS = ['Model', 'View', 'Controller', 'Config'];
+
+//     public static function load($clase)
+//     {
+//         foreach (self::CARPETAS as $carpeta) {
+//             if (file_exists("classes/$carpeta/" . strtolower($clase) . '.class.php')) {
+//                 include "classes/$carpeta/" . strtolower($clase) . '.class.php';
+//                 return;
+//             }
+//         }
+//         //         throw new Exception("No s'ha trobat la definicio de la classe $clase");
+//     }
+
+//     public static function newload($clase)
+//     {
+//         foreach (self::CARPETAS as $carpeta) {
+//             if (file_exists("classes/$carpeta/" . $clase . '.php')) {
+//                 include "classes/$carpeta/" . $clase . '.php';
+//                 return;
+//             }
+//         }
+//         throw new Exception("No s'ha trobat la definicio de la classe $clase");
+//     }
+// }
