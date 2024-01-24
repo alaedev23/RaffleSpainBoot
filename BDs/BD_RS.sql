8486513b797f4395992a99a33062d5e295b0a046
polanmarc <polanmarc19@gmail.com>
2024-01-24 21:31:37 +0100

    Arreglar Facturacion (quitar card_id)

--- BDs/BD_RS.sql
+++ BDs/BD_RS.sql
@@ -31,10 +31,6 @@ CREATE TABLE IF NOT EXISTS `rsdb`.`client` (
   `poblation` VARCHAR(45) NOT NULL,
   `address` VARCHAR(100) NOT NULL,
   `sex` CHAR(1) NULL DEFAULT NULL,
-  `card_id` INT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`card_id`)
-  REFERENCES `rsdb`.`facturacio` (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
   UNIQUE INDEX `phone_UNIQUE` (`phone` ASC) VISIBLE)
